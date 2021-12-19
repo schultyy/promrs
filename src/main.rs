b842767e1e3dc97aa03bfed451b3f05e38d0c5ec
Jan Schulte <janschulte@fastmail.com>
2021-12-19 16:29:58 -0500

    Add warp to create an http endpoint for querying
    Endpoint is listening at localhost:3030/hello. Currently, this is only a
    dummy endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use storage::Storage;
+use log::{error, info};
 use tokio::{sync::mpsc, task};
+use warp::Filter;
+
+use storage::Storage;
 
 mod metrics;
 mod storage;
@@ -9,37 +12,36 @@ fn scrape_endpoint() -> &'static str {
     "http://localhost:9100/metrics"
 }
 
-/// Fetches metrics from node_exporter
 async fn fetch_metrics() -> Result<Vec<String>, Box<dyn std::error::Error>> {
-    let metric_payload = reqwest::get(scrape_endpoint()).await?.text().await?;
-    let metrics = metric_payload
-        .lines()
+    let metric_payload = reqwest::get(scrape_endpoint())
+        .await?
+        .text()
+        .await?;
+    let metrics = metric_payload.lines()
         .map(|s| s.to_string())
         .collect::<Vec<_>>();
     Ok(metrics)
 }
 
+#[derive(Debug)]
+enum Command {
+    Store(String)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
     let (tx, mut rx) = mpsc::channel(32);
 
     let manager = tokio::spawn(async move {
         let mut storage = Storage::new();
         while let Some(cmd) = rx.recv().await {
-            match storage.store(cmd) {
-                Ok(_) => {
-                    println!("Stored metric");
-                }
-                Err(err) => {
-                    eprintln!("ERR: {}", err.to_string());
-                }
-            }
+            process_command(cmd, &mut storage);
         }
     });
 
-    let forever = task::spawn(async move {
-        let mut interval_timer =
-            tokio::time::interval(chrono::Duration::seconds(5).to_std().unwrap());
+    let forever_fetch_metrics = task::spawn(async move {
+        let mut interval_timer = tokio::time::interval(chrono::Duration::seconds(5).to_std().unwrap());
         loop {
             // Wait for the next interval tick
             interval_timer.tick().await;
@@ -47,15 +49,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             tokio::spawn(async move {
                 let results = fetch_metrics().await.unwrap();
                 for result in results {
-                    if let Err(err) = tx.send(result).await {
+                    if let Err(err) = tx.send(Command::Store(result)).await {
                         eprintln!("Encountered Error {:?}", err);
                     }
                 }
             });
         }
     });
+
+    let forever_http_interface = task::spawn(async move {
+        let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+        warp::serve(hello)
+            .run(([127, 0, 0, 1], 3030))
+            .await;
+    });
+
     manager.await.unwrap();
-    forever.await.unwrap();
+    forever_fetch_metrics.await.unwrap();
+    forever_http_interface.await.unwrap();
 
     Ok(())
+}
+
+fn process_command(cmd: Command, storage: &mut Storage) {
+    match cmd {
+        Command::Store(cmd) => {
+            store_data(storage, cmd);
+        }
+    }
+}
+
+fn store_data(storage: &mut Storage, cmd: String) {
+    match storage.store(cmd.to_string()) {
+        Ok(()) => {
+            info!("Stored {}", cmd);
+        }
+        Err(err) => {
+            error!("{}", err.to_string());
+        }
+    }
 }
