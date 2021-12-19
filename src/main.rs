8281db80889fd9a4587d8b9964f91f778e1643ce
Jan Schulte <janschulte@fastmail.com>
2021-12-19 16:44:44 -0500

    Broadcast store messages

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, convert::Infallible};
 
 use log::{debug, error, info};
-use tokio::{sync::mpsc, task};
+use tokio::{sync::{broadcast::{self, Receiver, Sender}, mpsc}, task};
 use warp::{
     http::{Response, StatusCode},
     Filter,
@@ -28,7 +28,7 @@ async fn fetch_metrics() -> Result<Vec<String>, Box<dyn std::error::Error>> {
     Ok(metrics)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Command {
     Store(String)
 }
@@ -36,11 +36,17 @@ enum Command {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-    let (tx, mut rx) = mpsc::channel(32);
+    // let (tx, mut rx) = mpsc::channel(32);
+    let vals: (Sender<Command>, Receiver<Command>) = broadcast::channel(500);
+    let tx = vals.0;
+    let mut rx1 = vals.1;
+    // let mut rx2 = tx.subscribe();
 
     let manager = tokio::spawn(async move {
         let mut storage = Storage::new();
-        while let Some(cmd) = rx.recv().await {
+        debug!("Spawn resource manager task");
+        while let Ok(cmd) = rx1.recv().await {
+            info!("Received Command {:?}", cmd);
             process_command(cmd, &mut storage);
         }
     });
@@ -51,10 +57,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             // Wait for the next interval tick
             interval_timer.tick().await;
             let tx = tx.clone();
+            info!("Fetching Metrics");
             tokio::spawn(async move {
                 let results = fetch_metrics().await.unwrap();
                 for result in results {
-                    if let Err(err) = tx.send(Command::Store(result)).await {
+                    // info!("Sending Metric \"{}\"", result);
+                    if let Err(err) = tx.send(Command::Store(result)) {
                         eprintln!("Encountered Error {:?}", err);
                     }
                 }
@@ -65,15 +73,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let forever_http_interface = task::spawn(async move {
         let hello = warp::path!("query")
         .and(warp::query::<HashMap<String, String>>())
-        .map(|p: HashMap<String, String>| {
-            info!("handling query request");
-            match p.get("key") {
-                Some(key) => {
-                    Response::builder().body(format!("key = {}", key))
-                },
-                None => Response::builder().body(String::from("No \"key\" param in query.")),
-            }
-        });
+        .and_then(get_query);
 
         info!("Listening at http://localhost:3030/query");
 
@@ -87,6 +87,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     forever_http_interface.await.unwrap();
 
     Ok(())
+}
+
+async fn get_query(p: HashMap<String, String>) -> Result<impl warp::Reply, Infallible> {
+    info!("handling query request");
+    Ok(format!("I waited seconds!"))
+    // match p.get("key") {
+    //     Some(query_str) => {
+    //         tx.send(Command::Query(query_str)).await;
+    //         Response::builder().body(format!("key = {}", key))
+    //     },
+    //     None => Response::builder().body(String::from("No \"key\" param in query.")),
+    // }
 }
 
 fn process_command(cmd: Command, storage: &mut Storage) {
