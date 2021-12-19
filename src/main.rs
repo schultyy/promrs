f4a054b7002ae72654d3204475afa4c59585a7be
Jan Schulte <janschulte@fastmail.com>
2021-12-19 14:52:09 -0500

    Fetch node_exporter metrics
    And print them out to stdout for now. We expect node_exporter to run at
    localhost:9100/metrics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn scrape_endpoint() -> &'static str {
+    "http://localhost:9100/metrics"
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let metrics = reqwest::get(scrape_endpoint())
+        .await?
+        .text()
+        .await?;
+    println!("{:#?}", metrics);
+    Ok(())
 }
