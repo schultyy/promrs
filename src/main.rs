f4cf8a04b0e3ceb3e7978dcfd4f5eb9798232552
Jan Schulte <janschulte@fastmail.com>
2021-12-19 14:55:58 -0500

    Fetch metrics periodically
    We now leverage chrono and tokio to fetch metrics every five seconds

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,10 @@ fn scrape_endpoint() -> &'static str {
     "http://localhost:9100/metrics"
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let metric_payload = reqwest::get(scrape_endpoint())
-        .await?
-        .text()
-        .await?;
+/// Fetches metrics from node_exporter and parses them
+/// into structs
+async fn fetch_metrics() -> Result<(), Box<dyn std::error::Error>> {
+    let metric_payload = reqwest::get(scrape_endpoint()).await?.text().await?;
     let metrics = metric_payload
         .lines()
         .map(|line| metrics::Metric::from_str(line))
@@ -18,4 +16,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .collect::<Vec<_>>();
     println!("{:?}", metrics);
     Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //We want a tick every five seconds
+    let mut interval_timer = tokio::time::interval(chrono::Duration::seconds(5).to_std().unwrap());
+    loop {
+        // Wait for the next interval tick
+        interval_timer.tick().await;
+        tokio::spawn(async {
+            let result = fetch_metrics().await;
+            if let Err(err) = result {
+                eprintln!("ERR: {:?}", err);
+            }
+        });
+    }
 }
